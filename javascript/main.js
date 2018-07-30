7b769eca73bae7d4ad3964fc228eb2f6e320081e
velezvanessa28 <velezvanessa193@gmail.com>
2018-07-30 18:26:58 -0500

    ahora se guarda toda la base de datos completa

--- javascript/main.js
+++ javascript/main.js
@@ -46,19 +46,33 @@ function edita(obj) {
 }
 
 
-$('#guardar').click(function(){
-  $( "td" ).each(function(guarda_datos) {
-console.log(inp);
-});
-  var datos={
-  horas1:$('.horas input').val(),
-  lunes:$('.lunes input').val(),
-  martes:$('.martes input').val(),
-  miercoles:$('.miercoles input').val(),
-  jueves:$('.jueves input').val(),
-  viernes:$('.viernes input').val(),
-  sabado:$('.sabado input').val(),
+$('#guardar').on("click", function(){
+
+  var horas = $('.horas input');
+  var horasVal = { "h1": horas[0].value, "h2": horas[1].value, "h3": horas[2].value, "h4": horas[3].value, "h5": horas[4].value, "h6": horas[5].value};
+  var lunes = $('.lunes input');
+  var lunesVal = {"l1": lunes[0].value, "l2": lunes[1].value, "l3": lunes[2].value, "l4": lunes[3].value, "l5": lunes[4].value, "l6": lunes[5].value,};
+  var martes = $('.martes input');
+  var martesVal = {"m1": martes[0].value, "m2": martes[1].value, "m3": martes[2].value, "m4": martes[3].value, "m5": martes[4].value, "m6": martes[5].value,};
+  var miercoles = $('.miercoles input');
+  var miercolesVal = {"mier1": miercoles[0].value, "mier2": miercoles[1].value, "mier3": miercoles[2].value, "mier4": miercoles[3].value, "mier5": miercoles[4].value, "mier6": miercoles[5].value,};
+  var jueves = $('.jueves input');
+  var juevesVal = {"j1": jueves[0].value, "j2": jueves[1].value, "j3": jueves[2].value, "j4": jueves[3].value, "j5": jueves[4].value, "j6": jueves[5].value,};
+  var viernes = $('.viernes input');
+  var viernesVal = {"v1": viernes[0].value, "v2": viernes[1].value, "v3": viernes[2].value, "v4": viernes[3].value, "v5": viernes[4].value, "v6": viernes[5].value,};
+  var sabado = $('.sabado input');
+  var sabadoVal = {"s1": sabado[0].value, "s2": sabado[1].value, "s3": sabado[2].value, "s4": sabado[3].value, "s5": sabado[4].value, "s6": sabado[5].value,};
+
+var datos={
+  horas: horasVal,
+  lunes: lunesVal,
+  martes: martesVal,
+  miercoles: miercolesVal,
+  jueves: juevesVal,
+  viernes: viernesVal,
+  sabado: sabadoVal,
 
 };
   firebase.database().ref("horas").push(datos);
-})
+
+});
